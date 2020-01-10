aaec6033b5106affc985215e762cacc0ea1f7850
Ryan Noronha <ryannoronha@Ryans-MacBook-Pro.local>
2020-01-10 18:04:39 -0300

    flask setup

---
+++ app.py
@@ -0,0 +1,10 @@
+import requests
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy 
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route('/')
+def index():
+    return render_template('weather.html')
